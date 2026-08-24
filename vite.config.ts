e42b973ebd82b476158304bc54c14b344581743b
apghalsasidev <a.p.ghalsasi@gmail.com>
2026-08-24 15:46:58 +0530

    chore(playwright-config) eslint error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,10 @@ export default defineConfig({
       globals: true,
       environment: "jsdom",
       setupFiles: "./src/test/setup.ts",
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/tests/e2e/**',
+      ],
   }),
 });
